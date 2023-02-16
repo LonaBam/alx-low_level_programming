8ad41bc40626a074493ce0a2bf9768724ffdb9e6
LonaBam <lonabam3@gmail.com>
2023-02-16 14:32:00 -0800

    Uses write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,5 @@
  */
 int main(void)
 {
-	cerr << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 }
