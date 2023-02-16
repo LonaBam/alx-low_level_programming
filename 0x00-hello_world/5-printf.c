4f8b90e6a9dabed582cb0656dba328f1e407ccda
LonaBam <lonabam3@gmail.com>
2023-02-16 12:33:51 -0800

    Adds flags

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,%s\n");
 
 	return (0);
 }
