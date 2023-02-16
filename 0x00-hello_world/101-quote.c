4635d496c9b1cc0acfd41f515f67cdb8068d9d5e
LonaBam <lonabam3@gmail.com>
2023-02-16 14:43:19 -0800

    Uses fwrite

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,5 +7,9 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fwrite(str, 1, sizeof(str), stderr);
+
+	return (0);
 }
