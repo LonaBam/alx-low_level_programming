6707044fac60efa1c4100aead5afe3fc6680fd45
LonaBam <lonabam3@gmail.com>
2023-02-16 15:11:59 -0800

    Rearranges fwrite function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fwrite(str"\n", 1, sizeof(str), stderr);
+	fwrite("\n", sizeof(str), 1, stderr);
 
 	return (1);
 }
