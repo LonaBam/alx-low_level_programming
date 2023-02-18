d237558136490eadb1258eba77780175b1a1e184
LonaBam <lonabam3@gmail.com>
2023-02-17 17:48:54 -0800

    Adds code that prints the alphabet except for two letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase excluding 2 letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || 'q')
+		{
+			letter = letter + 1;
+		} else
+			putchar(letter);
+			letter = letter + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1-last_digit.c - This program assigns a random number to a variable and prints the last digit of the number
 2-print_alphabet.c - This code prints the alphabet in lowercase
 3-print_alphabets.c - This code prints the alphabet in lowercase and then in uppercase
+4-print_alphabet.c - This code prints the alphabet in lowercase except for 2 letters
