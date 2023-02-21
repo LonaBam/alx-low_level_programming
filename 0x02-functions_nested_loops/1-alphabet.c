366f382da78bb32f9c60322d29c80bd1aaa93873
LonaBam <lonabam3@gmail.com>
2023-02-21 10:59:29 -0800

    Renames first function in main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
+/**
+ * main - Invoke code
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	print_alphabet();
+
+	return  (0);
 }
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int print_putc(void)
 {
 	puts("_putchar");
 
